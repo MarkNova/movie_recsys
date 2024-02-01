630de60958ac50c99f0e34705a9f9ded51c2079d
MarkNova <minutecoffe@mail.ru>
2024-02-01 22:11:44 +0300

    work with imdb ids tutorial

--- tutorial.py
+++ tutorial.py
@@ -2,11 +2,19 @@ from main import *
 
 recommender = Recommender()
 recommendations = recommender.model_recommendations(
-                                                [86320], #imdb id просмотренных фильмов
+                                                [86320], #id просмотренных фильмов
                                                 [5], #оценки фильмов
-                                                N=10 #количество рекоммендаций
+                                                N=10 
                                                 )
-print(recommendations) #внутренние id
+print(recommendations) #id рекоммендаций
 
+
+# вот так, если передаёшь imdb id:
+imdb_map = load_imdb_map()
+recommendations = recommender.model_recommendations(
+                                                imdb_map.from_imdb([1527186]), #imdb id просмотренных фильмов
+                                                [5], #оценки фильмов
+                                                N=10 
+                                                )
 print(load_imdb_map().to_imdb(recommendations)) #imdb id
 
